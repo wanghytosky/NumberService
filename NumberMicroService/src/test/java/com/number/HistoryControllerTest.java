b7cb0e0bcf14fe0b41e6812e4f819bb53f755df4
Wang HanY <wanghytosky@gmail.com>
2018-09-09 23:28:10 +0800

    delete them comment symbol

--- NumberMicroService/src/test/java/com/number/HistoryControllerTest.java
+++ NumberMicroService/src/test/java/com/number/HistoryControllerTest.java
@@ -10,19 +10,19 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HistoryControllerTest {
 	
-//	private MockMvc mvc;
-//
-//    @Test
-//    public void searchHistoryTest() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/history/search/3")
-//                    .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                    .accept(MediaType.APPLICATION_JSON_UTF8)
-//            )
-//           .andExpect(MockMvcResultMatchers.status().isOk())
-//           .andDo(MockMvcResultHandlers.print());
-//    }
+	private MockMvc mvc;
+
+    @Test
+    public void searchHistoryTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/history/search/3")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_JSON_UTF8)
+            )
+           .andExpect(MockMvcResultMatchers.status().isOk())
+           .andDo(MockMvcResultHandlers.print());
+    }
 }
