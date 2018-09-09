4f977467c3981dd8741df6b222c18221aa0079a1
wanghytosky <wanghytosky@gmail.com>
2018-09-09 22:58:47 +0800

    modify the images

---
+++ NumberMicroService/src/test/java/com/number/HistoryControllerTest.java
@@ -0,0 +1,28 @@
+package com.number;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class HistoryControllerTest {
+	
+//	private MockMvc mvc;
+//
+//    @Test
+//    public void searchHistoryTest() throws Exception {
+//        mvc.perform(MockMvcRequestBuilders.get("/history/search/3")
+//                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                    .accept(MediaType.APPLICATION_JSON_UTF8)
+//            )
+//           .andExpect(MockMvcResultMatchers.status().isOk())
+//           .andDo(MockMvcResultHandlers.print());
+//    }
+}
